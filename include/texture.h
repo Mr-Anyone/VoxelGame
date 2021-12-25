7d1659eb9b91f56c38b071c87287a1f7b194ff04
Mr-Anyone <53135664+Mr-Anyone@users.noreply.github.com>
2021-12-25 12:06:50 -0500

    Using a texture atlas

---
+++ include/texture.h
@@ -0,0 +1,18 @@
+#ifndef TEXTURE_H
+#define TEXTURE_H
+
+// Textures for block faces
+enum BlockFaceTexture
+{
+    FRONT,
+    BACK,
+    TOP,
+    BOTTOM,
+    LEFT,
+    RIGHT
+};
+
+// Texture Coordinate
+enum
+
+#endif
